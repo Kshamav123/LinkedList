96440ec3c73b75088c5bc014aa5caeba58e8459b
Kshamav123 <kshamavidya1999@gmail.com>
2021-09-12 11:05:56 +0530

    Deleting using pop

--- Main.java
+++ Main.java
@@ -5,8 +5,10 @@ public class Main {
         {
         	linkedlist list1= new linkedlist();
         	list1.add(56);
+        	list1.add(30);
         	list1.add(70);
-                list1.insertAtPos(2, 30);
+        	list1.pop(56);
+        	//list1.insertAtPos(3, 40);
         	list1.print();
         }
         
